259fa9f1a8788dacc8261a6039d72f62e0afcac8
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2018-01-17 01:23:29 +0800

    Change some algorithm

--- Hash Table--Two Sum/two_sum.cpp
+++ Hash Table--Two Sum/two_sum.cpp
@@ -9,24 +9,32 @@
 #include "two_sum.hpp"
 #include <map>
 #include <vector>
+#include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
+static const auto _ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> namesp;
-        vector<int> arr;
-        for (int index = 0; index < arr.size() - 1; index++) {
-            if (namesp.find(nums[index]) != namesp.end()){
-                arr.insert(arr.end(), namesp[nums[index]]);
-                arr.insert(arr.end(), index);
-                return arr;
-            }else{
-                namesp[target - nums[index]] = index;
-            }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        using SizeType = remove_reference_t<decltype(nums)>::size_type;
+using ValueToIndexMapType = unordered_map<int, SizeType>;
+        ValueToIndexMapType map;
+        vector<int> indices(2);
+for (SizeType index = 0; index < nums.size(); ++index)
+{
+const auto foundIterator = map.find(target - nums[index]);
+if (foundIterator != end(map) && foundIterator->second != index)
+    return vector<int>{ static_cast<int>(index), static_cast<int>(foundIterator->second) };
+    else map.emplace(nums[index], index);
         }
-        arr.clear();
-        return arr;
+        throw std::runtime_error("Solution not found");
     }
 };
